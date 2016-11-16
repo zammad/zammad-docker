59722e6006a10e65c6af1abb9a6fcb926ef91df8
André Bauer <abauer@magix.net>
2016-11-16 10:41:23 +0100

    removed everything from setup.sh which is done by the package now

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -8,18 +8,6 @@ yum -y install epel-release
 yum -y install https://download.postgresql.org/pub/repos/yum/9.6/redhat/rhel-6-x86_64/pgdg-redhat96-9.6-3.noarch.rpm
 yum -y install postgresql96 postgresql96-devel postgresql96-server postfix elasticsearch java cronie nginx which zammad
 
-su - zammad /bin/bash -l -c "cd /opt/zammad && cp config/database.yml.dist config/database.yml"
-
-su - zammad /bin/bash -l -c "cd /opt/zammad && echo 'production:' > config/database.yml"
-su - zammad /bin/bash -l -c "cd /opt/zammad && echo '  adapter: postgresql' >> config/database.yml"
-su - zammad /bin/bash -l -c "cd /opt/zammad && echo '  database: zammad' >> config/database.yml"
-su - zammad /bin/bash -l -c "cd /opt/zammad && echo '  pool: 50' >> config/database.yml"
-su - zammad /bin/bash -l -c "cd /opt/zammad && echo '  timeout: 5000' >> config/database.yml"
-su - zammad /bin/bash -l -c "cd /opt/zammad && echo '  username: zammad' >> config/database.yml"
-su - zammad /bin/bash -l -c "cd /opt/zammad && echo '  password:' >> config/database.yml"
-
-# setup DB
-/bin/bash -l -c "service postgresql-9.6 initdb"
 /bin/bash -l -c "echo 'max_connections = 200' >> /var/lib/pgsql/9.6/data/postgresql.conf"
 /bin/bash -l -c "echo 'shared_buffers = 2GB' >> /var/lib/pgsql/9.6/data/postgresql.conf"
 /bin/bash -l -c "echo 'temp_buffers = 1GB' >> /var/lib/pgsql/9.6/data/postgresql.conf"
@@ -32,13 +20,9 @@ su - zammad /bin/bash -l -c "cd /opt/zammad && echo '  password:' >> config/data
 /bin/bash -l -c "service postgresql-9.6 start && su - zammad -c 'export RAILS_ENV=production && cd /opt/zammad && export PATH=/opt/zammad/bin:$PATH && export GEM_PATH=/opt/zammad/vendor/bundle/ruby/2.3.0/ && rake db:create && rake db:migrate && rake db:seed'"
 
 # TMP FIX set up nginx (should be own package)
-/bin/bash -l -c "rm -rf /etc/nginx/conf.d/*"
 su - zammad /bin/bash -l -c "sed -i.bak '/server_name\syour\.domain\.org;/d' /opt/zammad/contrib/nginx/sites-enabled/zammad.conf"
-/bin/bash -l -c "cp /opt/zammad/contrib/nginx/sites-enabled/zammad.conf /etc/nginx/conf.d/"
-
 
 /bin/bash -l -c "cd /usr/share/elasticsearch && bin/plugin -install elasticsearch/elasticsearch-mapper-attachments/2.5.0"
-
 
 chmod +x /tmp/setup.sh
 chown zammad /tmp/setup.sh

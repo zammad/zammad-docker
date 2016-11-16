533c66550eb01bc19661db4fdfb0d5221b68e132
André Bauer <abauer@magix.net>
2016-11-16 20:48:12 +0100

    fix buildscript

--- scripts/build_image.sh
+++ scripts/build_image.sh
@@ -2,6 +2,6 @@
 
 set -ex
 
-docker build --no-cache --pull -t monotek/zammad:latest .
+docker build --no-cache --pull -t zammad/zammad:latest .
 docker login -u zammad -p ${DOCKERHUB_PW}
 docker push zammad/zammad:latest

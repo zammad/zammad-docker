e60fe6a7db0a42c56ee6e0750184825541270042
André Bauer <abauer@magix.net>
2017-01-13 18:16:00 +0100

    removed git branch from push script

--- scripts/build_image.sh
+++ scripts/build_image.sh
@@ -3,17 +3,15 @@
 DOCKER_LOGIN="$1"
 DOCKER_PASS="$2"
 DOCKER_REPO="$3"
-GIT_BRANCH="$4"
 
-if [ -z $1 ] || [ -z $2 ] || [ -z $3 ] || [ -z $4 ] ; then
-    echo "DockerHub login, pass & repo and/or git branch missing!"
+if [ -z $1 ] || [ -z $2 ] || [ -z $3 ]  ; then
+    echo "DockerHub login, pass and/or repo missing!"
     echo "Please add as vars to build command!"
-    echo "example:              $0 dockerhub_login dockerhub_password dockerhub_repo git_branch"
-    echo "example for Zammad:   $0 zammad password zammad develop"
-    echo "git_branch can be stable, develop, stable-1.1 or anything else available @ https://github.com/zammad/zammad"
+    echo "example:              $0 dockerhub_login dockerhub_password dockerhub_repo"
+    echo "example for Zammad:   $0 zammad password zammad"
     exit 1
 fi
 
-docker build --no-cache --pull --build-arg ${GIT_BRANCH} --build-arg BUILD_DATE=$(date -u +”%Y-%m-%dT%H:%M:%SZ”) -t ${DOCKER_LOGIN}/${DOCKER_REPO}:${GIT_BRANCH} .
+docker build --no-cache --pull --build-arg ${GIT_BRANCH} --build-arg BUILD_DATE=$(date -u +”%Y-%m-%dT%H:%M:%SZ”) -t ${DOCKER_LOGIN}/${DOCKER_REPO}:latest .
 docker login -u ${DOCKER_LOGIN} -p ${DOCKER_PASS}
-docker push ${DOCKER_LOGIN}/${DOCKER_REPO}:${GIT_BRANCH}
+docker push ${DOCKER_LOGIN}/${DOCKER_REPO}:latest
